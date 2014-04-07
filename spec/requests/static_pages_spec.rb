b5d73386c02ae7ea841694151346e17ec4d7bc5e
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-04-08 00:07:29 +0100

    set up rudimentary help page views

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe "Home page" do
+  it "should have the content 'sample app' " do
+    visit '/static_pages/home'
+    expect(page).to have_content('Sample App')
+  end  
+end
 
-    it "should have the content 'sample app' " do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end  
-  end
-
+describe "Help page" do 
+  it "should have content 'help' " do 
+    visit 'static_pages/help'
+    expect(page).to have_content('Help')
+  end  
+end  
 
